2542b2a45e57fa8d0d4055b18c562adc62c14365
sansutiwary <109430269+sansutiwary@users.noreply.github.com>
2023-01-29 11:23:26 +0530

    Time: 19 ms (20.18%), Space: 5.9 MB (66.04%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int fib(int n) {
-        int ans;
+       
         if(n==0)
             return 0;
         if(n==1)
             return 1;
-        else
-             ans=fib(n-1)+fib(n-2);
         
-        return ans;
+        return fib(n-1)+fib(n-2);
     }
 };
