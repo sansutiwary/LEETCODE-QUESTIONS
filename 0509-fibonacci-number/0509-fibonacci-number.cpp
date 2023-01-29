eca74b340becd8ac4bcf51262f5678aa1eebf885
sansutiwary <109430269+sansutiwary@users.noreply.github.com>
2023-01-29 11:11:44 +0530

    Time: 16 ms (32.44%), Space: 6 MB (20.43%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -6,8 +6,8 @@ public:
             return 0;
         if(n==1)
             return 1;
-        
-        ans=fib(n-1)+fib(n-2);
+        else
+             ans=fib(n-1)+fib(n-2);
         
         return ans;
     }
